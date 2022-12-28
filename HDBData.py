d45955eb5fb0e90e6a3bb3787eff5799daa62b89
williamchew85 <williamchew85@gmail.com>
2022-12-29 00:05:14 +0800

    v2

--- HDBData.py
+++ HDBData.py
@@ -4,7 +4,7 @@ class HDBData:
   @staticmethod
   def getData(callback):
     # make a request to the HDB DataMall API to retrieve the HDB data
-    data = requests.get('https://api.data.gov.sg/v1/hdb/prices')
+    data = requests.get('https://api.data.gov.sg/v1/hdb/data')
     data = data.json()
 
     # process the data and return it to the callback function
@@ -14,6 +14,10 @@ class HDBData:
         'location': row['location'],
         'type': row['type'],
         'size': row['size'],
-        'price': row['price']
+        'sales_price': row['sales_price'],
+        'rental_price': row['rental_price'],
+        'demographics': row['demographics'],
+        'infrastructure': row['infrastructure'],
+        'building_details': row['building_details']
       })
     callback(processedData)
